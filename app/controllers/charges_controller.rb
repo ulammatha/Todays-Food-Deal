a73a1e0ae702479a0d35238580de6cb84d4a152c
ulammatha <udaykumar1117@gmail.com>
2015-09-11 16:06:04 -0700

    code tweaks

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -21,9 +21,8 @@ def create
   payment = Payment.new(payment_params)
 
   if payment.save
-    coupon = CouponGeneration.new(current_user.id, deal_id)
-    generated_coupon = coupon.create_coupon
-    flash[:success] = "your payment $#{amount/100} has been successfully processed and coupon_code is: #{generated_coupon.code}"
+    coupon = CouponGeneration.new(current_user.id, deal_id).create_coupon
+    flash[:success] = "your payment $#{amount/100} has been successfully processed and coupon_code is: #{coupon.code}"
     redirect_to "/"
   end
 
